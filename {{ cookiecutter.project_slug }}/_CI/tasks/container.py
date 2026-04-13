26cc32c35ece9d361fbd962b6055ebaa998011b5
Costas Tyfoxylos <ctyfoxylos@schubergphilis.com>
2026-04-13 16:44:16 +0200

    fix: override act daemon socket with standard Linux path for podman

--- {{ cookiecutter.project_slug }}/_CI/tasks/container.py
+++ {{ cookiecutter.project_slug }}/_CI/tasks/container.py
@@ -55,7 +55,7 @@ def act(context: Context) -> None:
             context,
             f'DOCKER_HOST=unix://{socket} act push -W {QA_WORKFLOW} --secret-file .secrets '
             f'--container-architecture linux/amd64 '
-            f'--container-daemon-socket ""',
+            f'--container-daemon-socket /var/run/docker.sock',
         )
     else:
         execute(
